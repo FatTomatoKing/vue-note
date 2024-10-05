882249c0875cf94a007c846fb11029fe6fa33f55
行走的派大星 <1562024170@qq.com>
2024-10-05 13:02:35 +0800

    第一次提交demo

---
+++ src/components/SchoolInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <!--  组件的结构 html-->
+  <div class="demo">
+    <h2>学校姓名：{{ schoolName }}</h2>
+    <h2>学校地址：{{ schoolAddress }}</h2>
+    <button @click="showName">点我展示学校名称</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'SchoolInfo',
+  data() {
+    return {
+      schoolName: "深圳雪像小学",
+      schoolAddress: "深圳市区雪象花园新村",
+    }
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName)
+    }
+  }
+};
+
+
+</script>
+
+<style>
+/* css 样式*/
+.demo {
+  background-color: orange;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  // 完成了將App 组件放入到容器中
+  // $mount = el:'#app'
   render: h => h(App),
+  // render(createElement){
+  //   return createElement("h1","你好啊");
+  // }
 }).$mount('#app')
